07aea726600c712d50f68fe7735b5800c6e08d1d
Tuan Le <tle@arrowheadgrp.com>
2019-06-17 15:15:40 -0700

    query history by user and game id

--- src/main/java/com/game/checkout/gamecheckout/controller/HistoryController.java
+++ src/main/java/com/game/checkout/gamecheckout/controller/HistoryController.java
@@ -3,6 +3,7 @@ package com.game.checkout.gamecheckout.controller;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.game.checkout.gamecheckout.domain.History;
 
+import com.game.checkout.gamecheckout.domain.Game;
+import com.game.checkout.gamecheckout.domain.History;
+import com.game.checkout.gamecheckout.domain.User;
+import com.game.checkout.gamecheckout.repository.GameRepository;
 import com.game.checkout.gamecheckout.repository.HistoryRepository;
+import com.game.checkout.gamecheckout.repository.UserRepository;
+import com.game.checkout.gamecheckout.service.HistoryService;
 
 @RestController
 // TODO: figure out how to add the server.address here to the CrossOrigin
@@ -25,8 +31,20 @@ public class HistoryController {
 	
 	private final HistoryRepository historyRepository;
 	
-	public HistoryController(HistoryRepository historyRepository) {
+	private final HistoryService historyService;
+	
+	private final UserRepository userRepository;
+	
+	private final GameRepository gameRepository;
+	
+	public HistoryController(HistoryRepository historyRepository,
+                             HistoryService historyService, 
+                             UserRepository userRepository,
+                             GameRepository gameRepository) {
 		this.historyRepository = historyRepository;
+		this.historyService = historyService;
+		this.userRepository = userRepository;
+		this.gameRepository = gameRepository;
 	}
 	
 	@GetMapping("/history")
@@ -43,6 +61,34 @@ public class HistoryController {
 	@PostMapping("/history")
 	public void setHistory(@RequestBody History history) {
 		this.historyRepository.save(history);
+	}
+	
+	@GetMapping("/history/user/{id}")
+	public ResponseEntity<List <History>> findAllByUserId(@PathVariable String id) {
+		
+		Optional<User> user = userRepository.findById(Long.valueOf(id));
+		
+		if (user.isPresent()) {
+			List <History> history = this.historyService.getHistoryByUser(user.get());
+			return new ResponseEntity<List<History>>(history, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@GetMapping("/history/game/{id}")
+	public ResponseEntity<List <History>> findAllByGameId(@PathVariable String id) {
+		
+		Optional<Game> game = gameRepository.findById(Long.valueOf(id));
+		
+		if (game.isPresent()) {
+			List <History> history = this.historyService.getHistoryByGame(game.get());
+			return new ResponseEntity<List<History>>(history, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
 	}
 
 }
--- src/main/java/com/game/checkout/gamecheckout/service/HistoryService.java
+++ src/main/java/com/game/checkout/gamecheckout/service/HistoryService.java
@@ -1,26 +1,43 @@
 package com.game.checkout.gamecheckout.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
+import com.game.checkout.gamecheckout.domain.Game;
+import com.game.checkout.gamecheckout.domain.History;
 import com.game.checkout.gamecheckout.domain.User;
 
-import com.game.checkout.gamecheckout.domain.History;
-import com.game.checkout.gamecheckout.repository.HistoryRepository;
-
+@Service
 public class HistoryService {
 
-	private HistoryRepository repo;
+	@PersistenceContext 
+	private EntityManager entityManager;
 	
-	HistoryService(HistoryRepository repo) {
-		this.repo = repo;
+	public List <History> getHistoryByUser(User user) {
+		return getHistoryByEntity(user, "user");
 	}
 	
+	public List <History> getHistoryByGame(Game game) {
+		return getHistoryByEntity(game, "game");
+	}
+	
+	private List <History> getHistoryByEntity(Object entity, String attrib) {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<History> query = builder.createQuery(History.class);
+		Root<History> historyRoot = query.from(History.class);
+		List<History> result = entityManager.createQuery(
+							query.select(historyRoot)
+							     .where(builder.equal(historyRoot.get(attrib), entity)))
+				                 .getResultStream().collect(Collectors.toList());
+		return result;
+	}
 	
 }
